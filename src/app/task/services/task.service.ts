316d220fa96cdb5fe5b8680aa2d636508027c6a6
Laura Hénocq <laura.henocq@gmail.com>
2025-03-11 13:42:40 +0100

    :sparkles: handle error

--- src/app/task/services/task.service.ts
+++ src/app/task/services/task.service.ts
@@ -1,10 +1,11 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateTaskDto } from '../models/create-task.dto';
 import { UpdateTaskDto } from '../models/update-task.dto';
 import { TaskEntity } from '../models/task.entity';
 import { Task as TaskModel } from '../models/schemas/task.schema';
 import { Model } from 'mongoose';
 import { InjectModel } from '@nestjs/mongoose';
+import { ObjectId } from 'mongodb';
 
 @Injectable()
 export class TaskService {
@@ -28,17 +29,19 @@ export class TaskService {
     return tasksToReturn;
   }
 
-  async getTask(id: string): Promise<TaskEntity | null> {
+  async getTask(id: string): Promise<TaskEntity> {
     const task = await this.taskModel.findById(id).exec();
 
-    return task ? new TaskEntity(
+    if (!task) throw new NotFoundException(`Task with id ${id} was not found`);
+    
+    return new TaskEntity(
       { 
         title: task.title,
         description: task.description,
         status: task.status, 
         id: task._id.toString() 
       }
-    ) : null;
+    );
   }
 
   async createTask(createTaskDto: CreateTaskDto): Promise<string> {
@@ -48,13 +51,14 @@ export class TaskService {
 
   async updateTask(id: string, updateTaskDto: UpdateTaskDto): Promise<string> {
     const updatedTask = await this.taskModel.findByIdAndUpdate(id, updateTaskDto).exec();
-    return updatedTask ? `Task with id ${id} has been updated` : 'Task not found';
+    if (!updatedTask) throw new NotFoundException(`Task with id ${id} was not found`);
+    return `Task with id ${id} has been updated` ;
 
   }
 
   async deleteTask(id: string): Promise<string> {
     const deletedTask = await this.taskModel.findByIdAndDelete(id);
-    if (!deletedTask) throw new Error(`Task with id ${id} is not found`);
+    if (!deletedTask) throw new NotFoundException(`Task with id ${id} was not found`);
 
     return `Task with id ${id} has been deleted`;
   }
